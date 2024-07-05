c4bc55b73bd57666521ebdf2db4ade52f4fd3678
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-07-05 20:26:25 +0500

    hlsearch 🔍

--- .config/nvim/lua/akbar/core/keymaps.lua
+++ .config/nvim/lua/akbar/core/keymaps.lua
@@ -2,16 +2,16 @@ local map = vim.keymap -- for conciseness
 
 -- setting key maps
 vim.g.mapleader = " " -- set leader key
-map.set("n", "<leader>vs", ":vsp<CR>") -- creates vertical split
-map.set("n", "<leader>hs", ":sp<CR>") -- creates horizontal split
-map.set("n", "<leader>q", ":q<CR>") -- write & quit
-map.set("n", "<leader>wq", ":wq<CR>") -- write & quit
-map.set("n", "<leader>l", ":Lazy<CR>") -- Lazy window
-map.set("n", "<leader>s", ":w<CR>") -- write / save
-map.set("n", "<leader>nhls", ":nohlsearch<CR>") -- clear highlighted search
-map.set("n", "vv", "ggvG") -- clear highlighted search
-map.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" }) -- escape from insert mode
+map.set("n", "<leader>vs", ":vsp<CR>", { desc = "creates vertical split" })
+map.set("n", "<leader>hs", ":sp<CR>", { desc = "creates horizontal split" })
+map.set("n", "<leader>q", ":q<CR>", { desc = "just quit" })
+map.set("n", "<leader>wq", ":wq<CR>", { desc = "write & quit" })
+map.set("n", "<leader>l", ":Lazy<CR>", { desc = "Lazy window" })
+map.set("n", "<leader>s", ":w<CR>", { desc = "write / save" })
+map.set("n", "<leader>nhls", ":nohlsearch<CR>", { desc = "clear highlighted search" })
+map.set("n", "vv", "ggvG", { desc = "clear highlighted search" })
+map.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- increment/decrement numbers
-map.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-map.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+map.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+map.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
