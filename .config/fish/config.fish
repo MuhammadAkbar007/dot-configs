a3acff723f17264c4f39e0751675c2177e743793
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-07-11 16:19:32 +0500

    fish hello 🎣

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+end
+
+# My aliases
+alias eap='cd; ./install_me/ideaEAP/bin/idea'
+alias ls='lsd'
+alias ex='exit'
+alias la='ls -a'
+alias del='rm -rf'
+alias tm='tmux new -s akbar'
+alias tma='tmux a'
+alias nv='nvim'
+alias inv='nvim $(fzf -m --preview="bat --color=always {}")'
+alias cl='clear'
+alias gp='git push origin main'
+alias gc='git clone'
+alias up='sudo dnf update -y; flatpak update -y'
+alias open='xdg-open'
+alias shut='shutdown now'
+alias reb='reboot'
+alias sdcv='sdcv -c'
+function dic
+	espeak-ng "$argv"
+	sdcv -c "$argv"
+end
+
+starship init fish | source
