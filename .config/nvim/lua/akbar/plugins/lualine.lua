de1a05df36a8bc9248ee528f6377735e542391fa
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-10-25 00:29:15 +0500

    bigger font 😃

--- .config/nvim/lua/akbar/plugins/lualine.lua
+++ .config/nvim/lua/akbar/plugins/lualine.lua
@@ -9,12 +9,12 @@ return {
 		lualine.setup({
 			options = {
 				icons_enabled = true,
-				--theme = "catppuccin",
-				--theme = "auto",
+				-- theme = "catppuccin",
+				-- theme = "auto",
 				theme = "powerline_dark",
 				globalstatus = true,
-				--section_separators = { left = "▊", right = "" },
-				--component_separators = "|",
+				-- section_separators = { left = "", right = "" },
+				-- component_separators = "|",
 			},
 			sections = {
 				lualine_x = {
