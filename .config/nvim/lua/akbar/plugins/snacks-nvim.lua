49bc4815a44fb40ae4ef7ad3ae4ab8282ea1f2c0
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-12-06 23:59:22 +0500

    code folding added 📂

--- .config/nvim/lua/akbar/plugins/snacks-nvim.lua
+++ .config/nvim/lua/akbar/plugins/snacks-nvim.lua
@@ -9,6 +9,20 @@ return {
 		words = { enabled = true },
 		notify = { enabled = true },
 		notifier = { enabled = true, timeout = 3000 },
+		statuscolumn = {
+			enabled = true,
+			left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+			right = { "fold", "git" }, -- priority of signs on the right (high to low)
+			folds = {
+				open = true, -- show open fold icons
+				git_hl = true, -- use Git Signs hl for fold icons
+			},
+			git = {
+				-- patterns to match Git signs
+				patterns = { "GitSign", "MiniDiffSign" },
+			},
+			refresh = 50, -- refresh at most every 50ms
+		},
 		styles = {
 			notification = {
 				wo = { wrap = true }, -- Wrap notifications
