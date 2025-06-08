2a1dbd2c3164b5ddcf65a51f4349baf9c390da6f
MuhammadAkbar007 <fotismenos007@mail.ru>
2025-06-08 15:17:48 +0500

    options added 🎠

--- .config/nvim/lua/akbar/core/options.lua
+++ .config/nvim/lua/akbar/core/options.lua
@@ -7,6 +7,7 @@ local opt = vim.opt -- for simplicity
 opt.mouse = "a" -- enable mouse support
 opt.clipboard = "unnamedplus" -- enable system clipboard
 opt.swapfile = false -- don't use swapfile
+opt.backup = false
 opt.completeopt = "menuone,noinsert,noselect" -- autocomplete options
 
 -----------------------------------------------------------
@@ -18,13 +19,19 @@ opt.showmatch = true -- highlight matching parenthesis
 opt.ignorecase = true -- ignore case letters when search
 opt.smartcase = true -- ignore lowercase for the whole pattern
 opt.termguicolors = true -- enable 24-bit RGB colors
+opt.hlsearch = false -- no hlsearch
+opt.incsearch = true -- incremental search
+opt.scrolloff = 7 -- show at least 7 lines while scrolling
+opt.signcolumn = "yes" -- always show signcolumn
+opt.updatetime = 50 -- nvim updatetime in ms
 
 -----------------------------------------------------------
 -- Tabs, indent
 -----------------------------------------------------------
-opt.tabstop = 4 -- 1 tab == 4 spaces
-opt.smartindent = true -- autoindent new lines
+opt.tabstop = 4 -- 1 tab == 4 spaces (visually)
+opt.softtabstop = 4 -- 1 tab == 4 spaces (insert)
 opt.shiftwidth = 4 -- Shift 4 spaces when tab
+opt.smartindent = true -- autoindent new lines
 opt.wrap = false -- wrap line
 
 -----------------------------------------------------------
