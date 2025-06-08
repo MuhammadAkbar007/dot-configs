68d5bcf7eaae8ef6c5586fb4bbb095233347e056
MuhammadAkbar007 <fotismenos007@mail.ru>
2025-06-08 14:57:37 +0500

    nvim key maps optimized 🎹

--- .config/nvim/lua/akbar/core/keymaps.lua
+++ .config/nvim/lua/akbar/core/keymaps.lua
@@ -1,21 +1,52 @@
-local map = vim.keymap -- for conciseness
+local map = vim.keymap.set
 
-vim.g.mapleader = " " -- set leader key
-map.set("n", "<Tab>", ":bn<CR>", { desc = "creates horizontal split" })
-map.set("n", "<S-Tab>", ":bp<CR>", { desc = "creates horizontal split" })
+-- Set leader key
+vim.g.mapleader = " "
 
-map.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-map.set("n", "vv", "gg0vG$", { desc = "select whole file" })
-map.set("n", "<leader>s", ":w<CR>", { desc = "write / save" })
-map.set("n", "<leader>q", ":q<CR>", { desc = "just quit" })
-map.set("n", "<leader>wq", ":wq<CR>", { desc = "write & quit" })
-map.set("n", "<leader>lw", ":Lazy<CR>", { desc = "Lazy window" })
-map.set("n", "<leader>mw", ":Mason<CR>", { desc = "Mason window" })
-map.set("n", "<leader>nh", ":nohlsearch<CR>", { desc = "clear highlighted search" })
-map.set("n", "<leader>dm", ":delmarks!<CR>", { desc = "delete all marks" })
-vim.keymap.set("n", "<leader>o", ":silent !nautilus %:p:h &<CR>", { desc = "open current file with nautilus" })
-vim.keymap.set("n", "<leader>xdg", ":silent !xdg-open %:p &<CR>", { desc = "xdg-open current file" })
+-- General keymaps
+map("n", "<Tab>", ":bn<CR>", { desc = "Next buffer" })
+map("n", "<S-Tab>", ":bp<CR>", { desc = "Previous buffer" })
+map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+map("n", "vv", "gg0vG$", { desc = "Select whole file" })
 
--- increment/decrement numbers
-map.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-map.set("n", "<leader>_", "<C-x>", { desc = "Decrement number" })
+-- File operations
+map("n", "<leader>s", ":w<CR>", { desc = "Save file" })
+map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+map("n", "<leader>sq", ":wq<CR>", { desc = "Save and quit" })
+
+-- Plugin windows
+map("n", "<leader>lw", ":Lazy<CR>", { desc = "Open Lazy window" })
+map("n", "<leader>mw", ":Mason<CR>", { desc = "Open Mason window" })
+
+-- Utility
+map("n", "<leader>nh", ":nohlsearch<CR>", { desc = "Clear search highlights" })
+map("n", "<leader>dm", ":delmarks!<CR>", { desc = "Delete all marks" })
+
+-- File system operations
+map("n", "<leader>o", ":silent !nautilus %:p:h &<CR>", { desc = "Open directory in file manager" })
+map("n", "<leader>xdg", ":silent !xdg-open %:p &<CR>", { desc = "Open current file with default app" })
+
+-- Number increment/decrement
+map("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+map("n", "<leader>_", "<C-x>", { desc = "Decrement number" })
+
+-- Text manipulation
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+map("n", "J", "mzJ`z", { desc = "Join lines preserving cursor position" })
+
+-- Search navigation
+map("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+map("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+
+-- Clipboard operations
+map("x", "<leader>p", [["_dP]], { desc = "Paste without yanking selection" })
+map({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete to void register" })
+
+-- Text replacement
+map(
+	"n",
+	"<leader>rp",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Replace all occurrences of current word" }
+)
